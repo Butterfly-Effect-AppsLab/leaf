6291a7d587e5d0dbb96ec473907e733e42bd173b
rajcok2 <rajcok.p@gmail.com>
2019-11-13 23:31:58 +0100

    App.js update

--- source/react-sources/src/App.js
+++ source/react-sources/src/App.js
@@ -1,35 +1,4 @@
-/*
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactt
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-
-*/
-
-import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -54,34 +23,6 @@ const useStyles = makeStyles(theme => ({
     width: 200,
   },
 }));
-
-/*
-export default function TextFields() {
-  const classes = useStyles();
-  const [values, setValues] = React.useState({
-    name: 'Cat in the Hat',
-    age: '',
-    multiline: 'Controlled',
-    currency: 'EUR',
-  });
-
-  const handleChange = name => event => {
-    setValues({ ...values, [name]: event.target.value });
-  };
-
-  return (
-    <form className={classes.container} noValidate autoComplete="off">
-      <TextField
-        id="standard-name"
-        label="Name"
-        className={classes.textField}
-        value={values.name}
-        onChange={handleChange('name')}
-        margin="normal"
-      />
-    );
-}
-*/
 
 function App() {
   const classes = useStyles();
