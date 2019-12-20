70253b37220fb97263ea81de1f71cb03b6cd2a93
rajcok2 <rajcok.p@gmail.com>
2019-12-20 23:40:30 +0100

    set general @app.route for all [sub]pages

--- source/flask-backend/main.py
+++ source/flask-backend/main.py
@@ -3,29 +3,10 @@ import flask
 app = flask.Flask("__main__")
 
 
-@app.route('/')
-def my_index():
-    return flask.render_template("index.html", token="Hello FLASK!")
-
-@app.route('/Otazky')
-def my_Otazky():
-    return flask.render_template("index.html", token="Hello FLASK!")
-
-@app.route('/Firmy')
-def my_Firmy():
-    return flask.render_template("index.html", token="Hello FLASK!")
-
-@app.route('/LCFirma')
-def my_LCFirma():
-    return flask.render_template("index.html", token="Hello FLASK!")
-
-@app.route('/LcKategorie')
-def my_LcKategorie():
-    return flask.render_template("index.html", token="Hello FLASK!")
-
-@app.route('/ProjectInfo')
-def project_info():
-    return flask.render_template("index.html", token="Hello FLASK!")
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return flask.render_template("index.html", token="Helloo FLASK!")
 
 
 if __name__ == '__main__':
