317bcce2f99c8ba10f510d71505edd77c0b3eae1
rajcok2 <rajcok.p@gmail.com>
2019-12-22 16:23:52 +0100

    added endpoints for all companies and for the specific company

--- source/flask_backend/main.py
+++ source/flask_backend/main.py
@@ -1,6 +1,13 @@
 import flask
+from sqlalchemy.orm import sessionmaker
+from source.flask_backend.database.db_conn import connect
+from source.flask_backend.database.db_models import Company
+import json
 
 app = flask.Flask("__main__")
+db_conn = connect()
+Session = sessionmaker(db_conn)
+session = Session()
 
 
 @app.route('/', defaults={'path': ''})
@@ -9,9 +16,32 @@ def catch_all(path):
     return flask.render_template("index.html", token=path)
 
 
-@app.route('/api/v1.0/companies/')
+@app.route('/api/v1.0/companies/', methods=['GET'])
 def get_companies():
-    pass
+    companies_data = session.query(Company)
+    companies_json = {'companies': []}
+    for company in companies_data:
+        company_json = {
+            'company_id': company.id,
+            'company_name': company.name,
+            'specialization': company.id_specialization
+        }
+        companies_json['companies'].append(company_json)
+    return json.dumps(companies_json)
+
+
+@app.route('/api/v1.0/companies/<int:company_id>', methods=['GET'])
+def get_company_info(company_id):
+    company_data = session.query(Company).filter(Company.id == company_id)
+    company_info_json = {}
+    for company in company_data:
+        company_info_json = {
+            'description': company.description,
+            'history': company.history,
+            'unique_val_prop': company.unique_val_prop,
+            'revenue': company.revenue
+        }
+    return json.dumps(company_info_json)
 
 
 if __name__ == '__main__':
