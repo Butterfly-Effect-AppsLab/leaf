2292f6bd4e2e5401682fc97d091b59254f8d4196
rajcok2 <rajcok.p@gmail.com>
2019-12-22 17:48:07 +0100

    added user_projects endpoint with login_reguired prep

--- source/flask_backend/main.py
+++ source/flask_backend/main.py
@@ -1,8 +1,9 @@
 import flask
 from sqlalchemy.orm import sessionmaker
 from source.flask_backend.database.db_conn import connect
-from source.flask_backend.database.db_models import Company
+from source.flask_backend.database.db_models import UserProject, Company
 import json
+# from flask_login import login_required
 
 app = flask.Flask("__main__")
 db_conn = connect()
@@ -21,27 +22,43 @@ def get_companies():
     companies_data = session.query(Company)
     companies_json = {'companies': []}
     for company in companies_data:
-        company_json = {
+        company_info = {
             'company_id': company.id,
             'company_name': company.name,
             'specialization': company.id_specialization
         }
-        companies_json['companies'].append(company_json)
+        companies_json['companies'].append(company_info)
     return json.dumps(companies_json)
 
 
-@app.route('/api/v1.0/companies/<int:company_id>', methods=['GET'])
-def get_company_info(company_id):
-    company_data = session.query(Company).filter(Company.id == company_id)
-    company_info_json = {}
+@app.route('/api/v1.0/companies/<int:id_company>', methods=['GET'])
+def get_company_info(id_company):
+    company_data = session.query(Company).filter(Company.id == id_company)
+    company_info = {}
     for company in company_data:
-        company_info_json = {
+        company_info = {
             'description': company.description,
             'story_behind': company.history,
             'unique_val_prop': company.unique_val_prop,
             'revenue': company.revenue
         }
-    return json.dumps(company_info_json)
+    return json.dumps(company_info)
+
+
+@app.route('/api/v1.0/user_projects/<int:id_user>', methods=['GET'])
+# @login_required
+def get_user_projects(id_user):
+    projects_data = session.query(UserProject).filter(UserProject.id_user == id_user)
+    projects_info_json = {'user_projects': []}
+    for project in projects_data:
+        project_info = {
+            'id': project.id,
+            'id_user': project.id_user,
+            'title': project.title,
+            'description': project.description
+        }
+        projects_info_json['user_projects'].append(project_info)
+    return json.dumps(projects_info_json)
 
 
 if __name__ == '__main__':
