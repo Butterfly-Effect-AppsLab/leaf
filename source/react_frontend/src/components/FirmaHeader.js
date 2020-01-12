9a502b8e69957aae4559bd58ae5831039ba1601d
Evka <eva.milovska@gmail.com>
2020-01-12 19:22:11 +0100

    update of style for LcKategorie

--- source/react_frontend/src/components/FirmaHeader.js
+++ source/react_frontend/src/components/FirmaHeader.js
@@ -16,14 +16,32 @@ const useStyles = makeStyles(theme => ({
   appBar: {
     position: 'fixed',
     maxHeight: '80px',
+    paddingLeft: '0px',
+    paddingRight: '0px',
+    paddingTop: '0px',
+    paddingBottom: '0px',
     top: 0,
   },
-  title: {
+  but: {
     fontSize: 20,
     fontWeight: "bold",
     color: "white",
-    textAlign: "center",
     borderRadius: '0%',
+    position: 'absolute',
+    left: 0 ,
+    top: 0,
+    paddingBottom: '15px',
+    paddingLeft: '10px',
+  },
+  title: {
+    width: '100%',
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+    borderRadius: '0%',
+    textAlign: 'center',
+    position: 'absolute',
+    paddingBottom: '15px',
   },
   head: {
     width: '100%',
@@ -33,28 +51,32 @@ const useStyles = makeStyles(theme => ({
     marginleft: '15px',
     marginRight: '15px',
     marginBottom: '30px',
+    display: 'inline-block',
+    height: '30px',
+    position: 'relative'
   },
 }));
 
 export default function DenseAppBar() {
   const classes = useStyles();
-
   return (
     <div>
       <AppBar color='secondary' className={classes.appBar}>
         <Toolbar>
-          <div>
-            <IconButton edge="start" className={classes.title}>
-              <ArrowBackIosIcon />
-              <Typography className={classes.card}>
-              Späť
-              </Typography>
-            </IconButton>
-          </div>
           <div className={classes.head} >
-            <Typography className={classes.title} variant="h6">
-              Čistý zúbok
-            </Typography>
+             <div className={classes.but} >
+              <IconButton className={classes.but} >
+                <ArrowBackIosIcon />
+                <Typography className={classes.card}>
+                  Späť
+                </Typography>
+              </IconButton>
+              </div>
+              <div className={classes.title} >
+                <Typography  variant="h6">
+                  Čistý zúbok
+                </Typography>
+              </div>
           </div>
         </Toolbar>
       </AppBar>
